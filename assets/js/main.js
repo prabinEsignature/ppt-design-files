1d6815758838adba18a4c6b401b6be5f5acb67dd
Sushovan bhattarai <dev13@esignature.com.np>
2024-08-20 09:43:17 +0545

    pages added

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,4 +43,30 @@ document.addEventListener("DOMContentLoaded", () => {
     sidebarOpenBtn.classList.remove("is-open");
     adminSidebar.classList.remove("show-sidebar");
   });
+
+  // PROFILE IMAGE SELECTION
+  const fileInput = document.getElementById("profile-select-input");
+  const imgPreview = document.getElementById("profile-preview-img");
+  const removeImgBtn = document.getElementById("remove-img-btn");
+  const defaultImgSrc = imgPreview.src;
+
+  fileInput.addEventListener("change", function (event) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        imgPreview.src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  });
+
+  imgPreview.parentElement.addEventListener("click", function () {
+    fileInput.click();
+  });
+
+  removeImgBtn.addEventListener("click", function () {
+    imgPreview.src = defaultImgSrc;
+    fileInput.value = "";
+  });
 });
